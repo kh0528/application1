c681782e91214d151c4cddfc63f23d4c681f9741
kh0528 <chuyangchuanbin586@gmail.com>
2022-04-19 12:06:58 +0000

    Finish3-10

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,9 +1,10 @@
 class List < ApplicationRecord
   has_one_attached :image
 
-
-    validates :title, presence: true
+  validates :title, presence: true
   validates :body, presence: true
+
+   # コメントアウトを外す
   validates :image, presence: true
 
 end
